2dfcf2bde51046e4b119650c37c552cc68436812
cleiton <djcleitonduarte@hotmail.com>
2021-02-26 22:47:12 -0400

    Entity Task

---
+++ src/main/java/com/todoList/entities/Task.java
@@ -0,0 +1,85 @@
+package com.todoList.entities;
+
+import java.io.Serializable;
+import java.util.Locale.Category;
+
+public class Task implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String description;
+	
+	private Category category;
+	private User user;
+	
+	public Task() {
+		
+	}
+
+	public Task(Long id, String description, Category category, User user) {
+		super();
+		this.id = id;
+		this.description = description;
+		this.category = category;
+		this.user = user;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Task other = (Task) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+}
